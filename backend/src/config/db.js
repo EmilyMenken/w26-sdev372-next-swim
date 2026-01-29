867664d7801ebbe99b9a12a061eb2c5a7124a21c
EmilyM <155667566+EmilyMenken@user.noreply.github.com>
2026-01-29 14:47:14 -0800

    Changed in the db.js the database name for VM

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,11 +1,11 @@
-require("dotenv").config(); // make sure this is at the top
+require("dotenv").config();
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
   host: "localhost",
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: "nextswim",
+  database: "nextSwim",
   waitForConnections: true,
   connectionLimit: 10
 });
