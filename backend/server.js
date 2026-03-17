a2812d71dbd6257249744b334b73724c7752ccda
EmilyM <155667566+EmilyMenken@user.noreply.github.com>
2026-03-17 14:28:18 -0700

    docker working! changes to server.js to make the links appear at the bottom of the terminal

--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,10 @@ const PORT = process.env.PORT || 3001;
   await initDb();
 
   app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  setTimeout(() => {
     console.log(">~~~~~~~~~~~Backend: http://localhost:3001/api/aquatic-resources ~~~~~~~~~~~<");
-    console.log(">~~~~~~~~~~~Frontend: http://localhost:5173 ~~~~~~~~~~~<")
-  });
+    console.log(">~~~~~~~~~~~Frontend: http://localhost:5173 ~~~~~~~~~~~<");
+  }, 3000);
+});
 
 })();
